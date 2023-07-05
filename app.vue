01fd65e41e64f1e39528c4ac0f30d7d755a90ff7
comfuture <comfuture@iplateia.com>
2023-07-06 00:24:51 +0900

    initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+type Data = {
+  message: string
+}
+const { data } = useFetch<Data>('/api')
+</script>
+<template>
+  <div>
+    Message from server: {{ data?.message }}
+  </div>
+</template>
