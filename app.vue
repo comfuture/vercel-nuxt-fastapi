c9e00be3fa367892e9b3427669a9435ee69e7abb
comfuture <comfuture@iplateia.com>
2023-07-06 19:04:10 +0900

    cleanup

--- app.vue
+++ app.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { useAPI } from '~/composables/use-api'
-
 type Data = {
   message: string
 }
@@ -9,7 +7,6 @@ const { data } = await useAPI<Data>(route.fullPath)
 </script>
 <template>
   <div>
-    serve from icn1
     Message from server: {{ data?.message }}
   </div>
 </template>
--- composables/use-api.ts
+++ composables/use-api.ts
@@ -1,8 +1,8 @@
 export const useAPI: typeof useFetch = (request, opts) => {
   const headers = useRequestHeaders()
-  const { $fetch } = useRequestEvent()
 
   // append /api to the request
+  // on server, we need to prepend the host from the request headers
   const base = process.server? `https://${headers.host}` ?? '' : ''
   const _reqT = typeof request === 'function'
     ? request()
@@ -10,8 +10,7 @@ export const useAPI: typeof useFetch = (request, opts) => {
   const newRequest = `${base}/api${unref(_reqT)}`
 
   return useFetch(newRequest, {
-    headers,
+    headers,  // pass all request headers
     ...opts,
-    // $fetch: process.server ? $fetch : undefined
   })
 }
