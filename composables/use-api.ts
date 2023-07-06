4e648f65bc1b7ba3903157ce740b1eacb30da8f1
comfuture <comfuture@iplateia.com>
2023-07-06 18:32:14 +0900

    test

--- composables/use-api.ts
+++ composables/use-api.ts
@@ -3,7 +3,7 @@ export const useAPI: typeof useFetch = (request, opts) => {
   const { $fetch } = useRequestEvent()
 
   // append /api to the request
-  const base = process.server? `https://${headers.host}` ?? '' : ''
+  const base = '' // process.server? `https://${headers.host}` ?? '' : ''
   const _reqT = typeof request === 'function'
     ? request()
     : unref(request);
@@ -12,6 +12,6 @@ export const useAPI: typeof useFetch = (request, opts) => {
   return useFetch(newRequest, {
     headers,
     ...opts,
-    $fetch: process.server ? $fetch : undefined
+    // $fetch: process.server ? $fetch : undefined
   })
 }
